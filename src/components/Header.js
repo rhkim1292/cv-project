3e0959738da0da19eef6def95760de4138f8bd33
Randy Kim <rhkim1292@gmail.com>
2022-12-17 01:19:32 -0800

    Implement the add link functionality
    Users will now be able to add links to the info section of the header.
    
    A method to delete and/or edit each link needs to be implemented.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,53 +7,65 @@ class Header extends Component {
 		this.state = {
 			name: 'YOUR NAME',
 			title: 'Your Title',
-			phone: '(123) 456-7890',
+			phone: '123-456-7890',
 			email: 'your@email.com',
 			links: [],
+			hovering: false,
 			editMode: false,
+			addLinkMode: false,
 		};
 		this.startEditMode = this.startEditMode.bind(this);
+		this.startAddLinkMode = this.startAddLinkMode.bind(this);
 		this.makeEditBtnVisible = this.makeEditBtnVisible.bind(this);
 		this.makeEditBtnInvisible = this.makeEditBtnInvisible.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleHeaderFormSubmit = this.handleHeaderFormSubmit.bind(this);
+		this.handleAddLink = this.handleAddLink.bind(this);
 	}
 
 	makeEditBtnVisible(e) {
-		if (this.state.editMode) return;
-		const editBtn = document.querySelector('header button.header-edit-btn');
-		editBtn.style.display = 'block';
+		this.setState({ hovering: true });
 	}
 
 	makeEditBtnInvisible(e) {
-		if (this.state.editMode) return;
-		const editBtn = document.querySelector('header button.header-edit-btn');
-		editBtn.style.display = 'none';
+		this.setState({ hovering: false });
 	}
 
 	startEditMode(e) {
-		const editBtn = document.querySelector('header button.header-edit-btn');
-		const submitBtn = document.querySelector(
-			'header form button.header-submit-btn'
-		);
 		this.setState({ editMode: true });
-		editBtn.style.display = 'none';
-		submitBtn.style.display = 'block';
 	}
 
-	handleSubmit(e) {
-		const submitBtn = document.querySelector(
-			'header form button.header-submit-btn'
-		);
+	startAddLinkMode(e) {
+		this.setState({ addLinkMode: true });
+	}
+
+	handleHeaderFormSubmit(e) {
 		e.preventDefault();
 		const formData = new FormData(e.target);
 		this.setState({
 			name: formData.get('name'),
 			title: formData.get('title'),
 			phone: formData.get('phone'),
-            email: formData.get('email'),
+			email: formData.get('email'),
 			editMode: false,
+			addLinkMode: false,
 		});
-		submitBtn.style.display = 'none';
+	}
+
+	handleAddLink(e) {
+		const linkTypeSelect = document.querySelector(
+			'div.add-link-container select#linkType'
+		);
+		const linkInput = document.querySelector(
+			'div.add-link-container input#link'
+		);
+
+		this.setState({
+			links: this.state.links.concat({
+				linkType: linkTypeSelect.value,
+				link: linkInput.value,
+			}),
+			addLinkMode: false,
+		});
 	}
 
 	render() {
@@ -63,7 +75,7 @@ class Header extends Component {
 				onMouseOut={this.makeEditBtnInvisible}
 			>
 				<div className="style-box"></div>
-				<form onSubmit={this.handleSubmit}>
+				<form onSubmit={this.handleHeaderFormSubmit}>
 					<div className="name-title-section">
 						{this.state.editMode ? (
 							<input
@@ -71,7 +83,8 @@ class Header extends Component {
 								id="nameInput"
 								name="name"
 								placeholder="YOUR NAME"
-							></input>
+								required
+							/>
 						) : (
 							<h1 className="name">{this.state.name}</h1>
 						)}
@@ -81,7 +94,8 @@ class Header extends Component {
 								id="titleInput"
 								name="title"
 								placeholder="Your Title"
-							></input>
+								required
+							/>
 						) : (
 							<h2 className="title">{this.state.title}</h2>
 						)}
@@ -95,8 +109,10 @@ class Header extends Component {
 										defaultValue={this.state.phone}
 										id="phoneInput"
 										name="phone"
-										placeholder="(123) 456-7890"
-									></input>
+										placeholder="123-456-7890"
+										pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+										required
+									/>
 								) : (
 									this.state.phone
 								)}
@@ -109,7 +125,8 @@ class Header extends Component {
 										id="emailInput"
 										name="email"
 										placeholder="your@email.com"
-									></input>
+										required
+									/>
 								) : (
 									this.state.email
 								)}
@@ -117,18 +134,51 @@ class Header extends Component {
 							{this.state.links.map((currLink, idx) => (
 								<li key={idx}>{currLink.link}</li>
 							))}
+							{this.state.editMode &&
+								(this.state.addLinkMode ? (
+									<div className="add-link-container">
+										<select name="link_type" id="linkType">
+											<option value="github">
+												GitHub
+											</option>
+											<option value="linkedin">
+												LinkedIn
+											</option>
+											<option value="other">Other</option>
+										</select>
+										<input name="link" id="link" />
+										<button
+											type="button"
+											onClick={this.handleAddLink}
+										>
+											Add
+										</button>
+									</div>
+								) : (
+									<button
+										id="addLinkBtn"
+										type="button"
+										onClick={this.startAddLinkMode}
+									>
+										Add Link
+									</button>
+								))}
 						</ul>
 					</div>
-					<button className="header-submit-btn" type="submit">
-						Submit
-					</button>
+					{this.state.editMode ? (
+						<button className="header-submit-btn" type="submit">
+							Submit
+						</button>
+					) : null}
 				</form>
-				<button
-					className="header-edit-btn"
-					onClick={this.startEditMode}
-				>
-					Edit
-				</button>
+				{this.state.hovering && !this.state.editMode ? (
+					<button
+						className="header-edit-btn"
+						onClick={this.startEditMode}
+					>
+						Edit
+					</button>
+				) : null}
 			</header>
 		);
 	}
