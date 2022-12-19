c6b79a71a7cac776a311fc3ad98ef8b7905f9baa
Randy Kim <rhkim1292@gmail.com>
2022-12-19 00:42:28 -0800

    Add edit button on hover for education section
    The edit button doesn't do anything yet, but it will allow the user to
    edit existing schools and add more schools.

--- src/components/Education.js
+++ src/components/Education.js
@@ -13,25 +13,60 @@ class Education extends Component {
 					field: 'Computer Science',
 					gradMonth: 'June',
 					gradYear: '2020',
+					hovering: false,
+					editMode: false,
 				},
 			],
 		};
+		this.makeEditBtnVisible = this.makeEditBtnVisible.bind(this);
+		this.makeEditBtnInvisible = this.makeEditBtnInvisible.bind(this);
+		this.startEditMode = this.startEditMode.bind(this);
 	}
+
+	makeEditBtnVisible(e) {
+		this.setState({ hovering: true });
+	}
+
+	makeEditBtnInvisible(e) {
+		this.setState({ hovering: false });
+	}
+
+	startEditMode(e) {
+		this.setState({ editMode: true });
+	}
+
 	render() {
 		return (
-			<div className="education-section">
+			<div
+				className="education-section"
+				onMouseOver={this.makeEditBtnVisible}
+                onMouseOut={this.makeEditBtnInvisible}
+			>
 				<h2>Education</h2>
 				{this.state.schools.map((currSchool, idx) => (
 					<div className="school-container">
 						<h3>
-							<b>{currSchool.name} | </b> {currSchool.location}<br />
-                            <div className="degree-field-grad-date">
-                                <p className="degree-field">{currSchool.degree} in {currSchool.field}</p>
-                                <p className="grad-date">{currSchool.gradMonth} {currSchool.gradYear}</p>
-                            </div>
+							<b>{currSchool.name} | </b> {currSchool.location}
+							<br />
+							<div className="degree-field-grad-date">
+								<p className="degree-field">
+									{currSchool.degree} in {currSchool.field}
+								</p>
+								<p className="grad-date">
+									{currSchool.gradMonth} {currSchool.gradYear}
+								</p>
+							</div>
 						</h3>
 					</div>
 				))}
+				{this.state.hovering && !this.state.editMode ? (
+					<button
+						className="education-edit-btn"
+						onClick={this.startEditMode}
+					>
+						Edit
+					</button>
+				) : null}
 			</div>
 		);
 	}
